42477c162e138b1baaf599e2c770a589ef796924
JimCannon <arinze.nwadinkpa@gmail.com>
2021-02-04 14:23:09 +0100

    working on homeView

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,7 +1,16 @@
+import "./HomeView.css";
+
 export const HomeView = () => {
-    return(
-        <div>
-            <h1>This is the Home view.</h1>
+  return (
+    <div className="home-container">
+      <div className="home-banner">
+        <div className="banner-content"></div>
+      </div>
+      <div className="news-content">
+        <div className="news-container-center">
+          <h1 className="news-title">SENASTE NYTT</h1>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
